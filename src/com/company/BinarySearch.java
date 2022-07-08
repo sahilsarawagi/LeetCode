7cb3e48dbfd2789bea70a67d47bfd8b24318914c
Sahil <sahilsarawagi1@gmail.com>
2022-07-08 23:14:01 +0530

    added BinarySearch

---
+++ src/com/company/BinarySearch.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr ={1,6,8,9,3,7,4};
+        System.out.println(search(arr,6));
+    }
+    public static int search(int[] nums, int target) {
+        int start=0;
+        int end= nums.length-1;
+        while(start<=end){
+            int mid= start+(end-start)/2;
+            if(target>nums[mid]){
+                start=mid+1;
+            }else if(target<nums[mid]){
+                end=mid-1;
+            }else{
+                return mid;
+            }
+
+        }
+        return -1;
+    }
+
+}
